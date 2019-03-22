b4b638c4cba8de0a873c73956c6ecb5477b13217
Nicky Croydon <nickc87@hotmail.co.uk>
2019-03-22 17:20:38 +0000

    init git repository and add files

---
+++ js_sandbox/app.js
@@ -0,0 +1,10 @@
+function reverse(str){
+ return str.split("").reverse().join("");
+// let splitStr = str.split("");
+// let revStr = splitStr.reverse();
+// let joinStr = revStr.join("");
+// return joinStr;
+}
+
+
+console.log(reverse('hello'));
