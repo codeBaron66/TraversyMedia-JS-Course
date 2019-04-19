f63e63fe33b7cea6f11d9302de10c004d365d440
Nicky Croydon <nickc87@hotmail.co.uk>
2019-04-19 12:19:33 +0100

    All logic for request added

--- Ajax & JSON/app.js
+++ Ajax & JSON/app.js
@@ -5,7 +5,19 @@ function loadCustomer(e) {
   xhr.open('GET', 'customer.json', true);
   xhr.onload = function() {
     if (this.status === 200) {
-      console.log(this.responseText);      
+      const customer = JSON.parse(this.responseText);
+      // console.log(customer);
+      const output = `
+        <ul>
+          <li>Name: ${customer.name}</li>
+          <li>ID: ${customer.id}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Name: ${customer.phone}</li>
+        </ul>
+      `;
+      // const customerDiv = document.getElementById('customer');
+      // customerDiv.innerHTML = output;
+      document.getElementById('customer').innerHTML = output;
     }
   }
   xhr.send();
