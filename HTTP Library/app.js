95727ca0a5588fa5cf483444628e97652a86c6ae
Nicky Croydon <nickc87@hotmail.co.uk>
2019-04-23 21:48:00 +0100

    Add POST, PUT and DELETE to prototype

--- HTTP Library/app.js
+++ HTTP Library/app.js
@@ -1,6 +1,45 @@
 const http = new easyhttp;
 
 // GET POSTS
-http.get('https://jsonplaceholder.typicode.com/posts', function(response) {
-  console.log(response);  
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if (err) {
+//     console.log(err);    
+//   } else {
+//     console.log(posts);
+    
+//   }
+// });
+
+// Create data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);    
+//   }
+// });
+
+// update post
+http.put('https://jsonplaceholder.typicode.com/posts/40', data, function(err, post) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(post);    
+  }
+});
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if (err) {
+    console.log(err);    
+  } else {
+    console.log(response);
+    
+  }
 });
